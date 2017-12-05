b814b27dad7b8dfdc51d26528f40d67c8de93a32
Peter Barker <pbarker@barker.dropbear.id.au>
2017-12-05 17:14:38 +1100

    Fixes to be squashed in

--- examples/set_attitude_target/set_attitude_target.py
+++ examples/set_attitude_target/set_attitude_target.py
@@ -3,9 +3,11 @@
 
 """
 
-change_attitude.py: (Copter Only)
+set_attitude_target.py: (Copter Only)
 
-This example shows how to move/direct Copter and send commands in GUIDED_NOGPS mode using DroneKit Python.
+This example shows how to move/direct Copter and send commands
+ in GUIDED_NOGPS mode using DroneKit Python.
+
 Caution: A lot of unexpected behaviors may occur in GUIDED_NOGPS mode.
         Always watch the drone movement, and make sure that you are in dangerless environment.
         Land the drone as soon as possible when it shows any unexpected behavior.
@@ -20,9 +22,9 @@ import time
 import math
 
 # Set up option parsing to get connection string
-import argparse  
+import argparse
 parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode ')
-parser.add_argument('--connect', 
+parser.add_argument('--connect',
                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
@@ -44,14 +46,15 @@ def arm_and_takeoff_nogps(aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude without GPS data.
     """
-    
+
     ##### CONSTANTS #####
     DEFAULT_TAKEOFF_THRUST = 0.7
     SMOOTH_TAKEOFF_THRUST = 0.6
-    
+
     print("Basic pre-arm checks")
     # Don't let the user try to arm until autopilot is ready
-    # If you need to disable the arming check, just comment it with your own responsibility.
+    # If you need to disable the arming check,
+    # just comment it with your own responsibility.
     while not vehicle.is_armable:
         print(" Waiting for vehicle to initialise...")
         time.sleep(1)
@@ -64,6 +67,7 @@ def arm_and_takeoff_nogps(aTargetAltitude):
 
     while not vehicle.armed:
         print(" Waiting for arming...")
+        vehicle.armed = True
         time.sleep(1)
 
     print("Taking off!")
@@ -71,7 +75,8 @@ def arm_and_takeoff_nogps(aTargetAltitude):
     thrust = DEFAULT_TAKEOFF_THRUST
     while True:
         current_altitude = vehicle.location.global_relative_frame.alt
-        print(" Altitude: %s" % current_altitude)
+        print(" Altitude: %f  Desired: %f" %
+              (current_altitude, aTargetAltitude))
         if current_altitude >= aTargetAltitude*0.95: # Trigger just below target alt.
             print("Reached target altitude")
             break
@@ -98,36 +103,22 @@ def set_attitude(roll_angle = 0.0, pitch_angle = 0.0, yaw_rate = 0.0, thrust = 0
     # Thrust == 0.5: Hold the altitude
     # Thrust <  0.5: Descend
     msg = vehicle.message_factory.set_attitude_target_encode(
-                                                             0,
-                                                             0,
-                                                                 # Target system
-                                                             0,
-                                                                 # Target component
-                                                             0b00000000,
-                                                                 # Type mask: bit 1 is LSB
-                                                             to_quaternion(roll_angle, pitch_angle),
-                                                                 # Quaternion
-                                                             0,
-                                                                 # Body roll rate in radian
-                                                             0,
-                                                                 # Body pitch rate in radian
-                                                             math.radians(yaw_rate),
-                                                                 # Body yaw rate in radian
-                                                             thrust)
-                                                                 # Thrust
+        0, # time_boot_ms
+        1, # Target system
+        1, # Target component
+        0b00000000, # Type mask: bit 1 is LSB
+        to_quaternion(roll_angle, pitch_angle), # Quaternion
+        0, # Body roll rate in radian
+        0, # Body pitch rate in radian
+        math.radians(yaw_rate), # Body yaw rate in radian
+        thrust  # Thrust
+    )
     vehicle.send_mavlink(msg)
-                                                             
-    if duration != 0:
-        # Divide the duration into the frational and integer parts
-        modf = math.modf(duration)
-        
-        # Sleep for the fractional part
-        time.sleep(modf[0])
-        
-        # Send command to vehicle on 1 Hz cycle
-        for x in range(0,int(modf[1])):
-            time.sleep(1)
-            vehicle.send_mavlink(msg)
+
+    start = time.time()
+    while time.time() - start < duration:
+        vehicle.send_mavlink(msg)
+        time.sleep(0.1)
 
 def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
     """
@@ -139,12 +130,12 @@ def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
     t3 = math.sin(math.radians(roll * 0.5))
     t4 = math.cos(math.radians(pitch * 0.5))
     t5 = math.sin(math.radians(pitch * 0.5))
-    
+
     w = t0 * t2 * t4 + t1 * t3 * t5
     x = t0 * t3 * t4 - t1 * t2 * t5
     y = t0 * t2 * t5 + t1 * t3 * t4
     z = t1 * t2 * t4 - t0 * t3 * t5
-    
+
     return [w, x, y, z]
 
 # Take off 2.5m in GUIDED_NOGPS mode.
@@ -163,10 +154,10 @@ set_attitude(duration = 3)
 # Move the drone forward and backward.
 # Note that it will be in front of original position due to inertia.
 print("Move forward")
-set_attitude(pitch_angle = 1, thrust = 0.5, duration = 3.21)
+set_attitude(pitch_angle = -5, thrust = 0.5, duration = 3.21)
 
 print("Move backward")
-set_attitude(pitch_angle = -1, thrust = 0.5, duration = 3)
+set_attitude(pitch_angle = 5, thrust = 0.5, duration = 3)
 
 
 print("Setting LAND mode...")
